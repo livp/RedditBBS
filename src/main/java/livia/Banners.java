b96c076905f9066b8266526af01fad9e1c0d512c
livia <livia@sfsu.edu>
2019-10-29 15:02:08 -0700

    Welcome banner done, initial prompt parsing.

--- src/main/java/livia/Banners.java
+++ src/main/java/livia/Banners.java
@@ -1,5 +1,10 @@
 package livia;
 
+import org.jline.terminal.Terminal;
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStringBuilder;
+import org.jline.utils.AttributedStyle;
+
 public class Banners {
 
   public static final String BIG_ALIEN =
@@ -24,23 +29,55 @@ public class Banners {
             + "      '²«?$.          .$?»²'\n"
             + "        'b            d'\n"
             + "      ,²²'?,.      .,?'²²,\n"
-            + "      ²==--≥²²==--==²²≤--==²";
+            + "      ²==--≥²²==--==²²≤--==²\n";
 
-  public static final String WELCOME = "Welcome to Reddit BBS.";
+  public static final String WELCOME_LINE = "Welcome to Reddit BBS";
 
-  // Prevent instances.
-  private Banners() {}
+  /**
+   * Gets the nice little alien shown in the terminal.
+   */
+  static void displayWelcomeBanner(Terminal terminal) {
+    AttributedString welcome = new AttributedStringBuilder()
+        .style(AttributedStyle.DEFAULT.bold())
+        .style(AttributedStyle.DEFAULT.foreground(AttributedStyle.RED))
+        .append(centerBanner(terminal, BIG_ALIEN))
+        .style(AttributedStyle.DEFAULT.boldOff())
+        .append(centerBanner(terminal, WELCOME_LINE))
+        .toAttributedString();
+    terminal.writer().println(welcome.toAnsi());
+    terminal.flush();
+  }
 
   /**
    * Computes the effective width of a multi-line string.
    */
   static int bannerWidth(String banner) {
-    int width = 0;
+    int bannerWidth = 0;
     for (String s : banner.split("\n")) {
-      if (s.length() > width) { width = s.length(); }
+      if (s.length() > bannerWidth) {
+        bannerWidth = s.length();
+      }
     }
-    return width;
+    return bannerWidth;
   }
+
+  /**
+   * Pads a banner with spaces to center it in the terminal.
+   */
+  static String centerBanner(Terminal terminal, String banner) {
+    int pad = Math.floorDiv(terminal.getWidth() - bannerWidth(banner), 2);
+    if (pad <= 0) {
+      return banner;
+    }
+    String padded = "";
+    for (String s : banner.split("\n")) {
+      padded += String.format("%" + pad + "c", ' ') + s + "\n";
+    }
+    return padded;
+  }
+
+  // Prevent instantiation.
+  private Banners() {}
 
 
 }
--- src/main/java/livia/RedditBBS.java
+++ src/main/java/livia/RedditBBS.java
@@ -15,20 +15,42 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 import org.fusesource.jansi.AnsiConsole;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.ParsedLine;
+import org.jline.reader.Parser;
+import org.jline.reader.impl.DefaultParser;
 import org.jline.terminal.Attributes;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import org.jline.utils.AttributedStringBuilder;
+import org.jline.utils.AttributedStyle;
+import org.jline.utils.InfoCmp.Capability;
 
 public class RedditBBS {
 
-  static HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-  static JsonFactory JSON_FACTORY = new JacksonFactory();
+  private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+  private static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
   private Terminal terminal = null;
+  private LineReader reader = null;
+  private Parser parser = new DefaultParser();
+  private String prompt = new AttributedStringBuilder()
+      .style(AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE))
+      .append("> ")
+      .style(AttributedStyle.DEFAULT.foreground(AttributedStyle.WHITE))
+      .toAnsi();
 
   public RedditBBS() {
     try {
       terminal = TerminalBuilder.builder().system(true).build();
+      terminal.puts(Capability.clear_screen);
+      terminal.flush();
+
+      reader = LineReaderBuilder.builder()
+          .terminal(terminal)
+          .parser(parser)
+          .build();
 
     } catch(IOException e) {
       System.err.println("Unable to open the terminal. Goodbye.");
@@ -36,16 +58,31 @@ public class RedditBBS {
     }
   }
 
-  /**
-   * Uninstall settings, kill threads.
-   */
-  void goodbye() {
-    AnsiConsole.systemUninstall();
+  void run() {
+    displayWelcomeBanner();
+
+    while(true) {
+      String line = reader.readLine(prompt);
+      ParsedLine parsedLine = reader.getParser().parse(line, 0);
+
+      terminal.writer().println("You said:\n");
+      boolean end = false;
+      for (String word : parsedLine.words()) {
+        terminal.writer().println(word);
+        if (word.toUpperCase().equals("END")) {
+          end = true;
+        }
+      }
+      terminal.flush();
+      if (end) {
+        System.exit(0);
+      }
+    }
   }
 
-  void displayWelcomeBanner() {
+  private void displayWelcomeBanner() {
 
-    terminal.writer().println(Banners.BIG_ALIEN);
+    Banners.displayWelcomeBanner(terminal);
     // AnsiConsole.out().print(
     //     ansi().eraseScreen().cursor(0,0));
     // AnsiConsole.out().println(ansi().fgBright(RED).a(Banners.BIG_ALIEN));
@@ -53,9 +90,7 @@ public class RedditBBS {
   }
 
   public static void main(String[] _ignored) {
-    RedditBBS bbs = new RedditBBS();
-    bbs.displayWelcomeBanner();
-    bbs.goodbye();
+    new RedditBBS().run();
   }
 
 }
